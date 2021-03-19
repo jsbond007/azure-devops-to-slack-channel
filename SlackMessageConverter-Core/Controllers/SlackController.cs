729d3a007d7b5be4b09ce79efc3d03c3d027a512
Net Core Bond <imfreeguys@gmail.com>
2021-03-19 15:20:39 +0530

    Initial Commit

---
+++ SlackMessageConverter-Core/Controllers/SlackController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace SlackMessageConverter.Core
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlackController : ControllerBase
+    {
+        [HttpPost]
+        public async void ConvertAndSend([FromBody] JObject data)
+        {
+
+
+            string user = this.Request.Query["user"].ToString();
+            string cid = this.Request.Query["channel"].ToString();
+            string token = this.Request.Query["token"].ToString();
+
+            string url = $"https://hooks.slack.com/services/{user}/{cid}/{token}";
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            //var data = JObject.Parse(body);
+
+
+            var oest = data.SelectToken("resource.fields.['Microsoft.VSTS.Scheduling.OriginalEstimate']");
+            var allFields = data.SelectToken("resource.fields").Where(p => p.Path.Contains("Critical"));
+
+            var attachFields = new List<AttachmentField>();
+
+
+            string postMessage = "";
+            postMessage = data.SelectToken("message.html")?.ToString();
+
+            if (oest != null)
+            {
+
+                attachFields.Add(new AttachmentField
+                {
+                    title = "Old Original Estimation",
+                    value = oest.SelectToken("oldValue")?.ToString()
+                });
+
+                attachFields.Add(new AttachmentField
+                {
+                    title = "New Original Estimation",
+                    value = oest.SelectToken("newValue")?.ToString()
+                });
+            }
+
+
+            foreach (var field in allFields)
+            {
+                if (data.SelectToken(field.Path + ".oldValue") != null)
+                {
+                    attachFields.Add(new AttachmentField
+                    {
+                        title = "Old " + GetFieldTitle(field.Path),
+                        value = data.SelectToken(field.Path + ".oldValue")?.ToString() ?? ""
+                    });
+                }
+
+                if (data.SelectToken(field.Path + ".newValue") != null)
+                {
+                    attachFields.Add(new AttachmentField
+                    {
+                        title = "New " + GetFieldTitle(field.Path),
+                        value = data.SelectToken(field.Path + ".newValue")?.ToString() ?? ""
+                    });
+                }
+            }
+
+
+            if (attachFields.Count > 0)
+            {
+                var html = data.SelectToken("message.markdown").ToString();
+                var taskBugIndex = html.IndexOf("]", 1);
+                var taskBug = html.Substring(1, taskBugIndex - 1);
+                var linkEndIndex = html.IndexOf(")", taskBugIndex + 2);
+                var link = html.Substring(taskBugIndex + 2, linkEndIndex - taskBugIndex - 2);
+                var text = html.Substring(linkEndIndex + 2);
+                var finalText = $"<{link}|{taskBug}>{text}";
+                await RestClient.SendToSlack(finalText, attachFields, url);
+            }
+
+
+        }
+
+        private string GetFieldTitle(string path)
+        {
+            string returnValue = path.Replace("resource.fields['", "").Replace("Custom.", "").Replace("Critical_", "").Replace("']", "");
+            return returnValue;
+        }
+
+
+    }
+
+    public class AttachmentField
+    {
+        public AttachmentField()
+        {
+            @short = true;
+        }
+
+        public string title { get; set; }
+        public string value { get; set; }
+        public bool @short { get; set; }
+    }
+
+    public class SlackMessage
+    {
+        public List<Attachment> attachments { get; set; }
+    }
+
+    public class Attachment
+    {
+        public List<AttachmentField> fields { get; set; }
+        public string pretext { get; set; }
+        
+    }
+
+}
---
+++ SlackMessageConverter-Core/RestClient.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackMessageConverter.Core
+{
+    public class RestClient
+    {
+        public async static Task SendToSlack(string strMessage, List<AttachmentField> fields, string url)
+        {
+
+           
+            SlackMessage message = new SlackMessage()
+            {
+                attachments = new List<Attachment>()
+            };
+
+            message.attachments.Add(new Attachment
+            {
+                fields = fields,
+                pretext = strMessage
+            });
+
+
+            var d = JsonConvert.SerializeObject(message);
+
+            await PostRequest(url, message);
+        }
+
+        private static HttpRequestMessage GetPostMessage(string url, dynamic data)
+        {
+            string json = JsonConvert.SerializeObject(data);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Content = content,
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(url),
+            };
+
+
+
+            //request.Headers.Add("contentType", "application/json");
+            return request;
+
+        }
+        private static HttpClient _defaultClient;
+        private static HttpClient GetHttpClient()
+        {
+            if (_defaultClient == null)
+            {
+                _defaultClient = new HttpClient();
+                _defaultClient.Timeout = new TimeSpan(2, 0, 5000);
+            }
+
+            return _defaultClient;
+        }
+
+        private static async Task<string> PostRequest(string url, dynamic data)
+        {
+            try
+            {
+                HttpClient client = GetHttpClient();
+                var request = GetPostMessage(url, data);
+
+              
+                var response = await client.SendAsync(request);
+
+                var responseJson = await response.Content.ReadAsStringAsync();
+                return responseJson;
+				
+            }
+            catch 
+            {
+                throw;                
+            }
+
+        }
+    }
+}
